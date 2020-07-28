d53dcd29098e6aa576d7c677fba955a03cbdd5fd
Adam Mathieson <mathieson1222@gmail.com>
2020-07-27 23:47:17 -0700

    first commit; removes boilerplate

---
+++ nrel-data/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NREL Data Visualization</h1>
+      <img src={logo} alt="react logo"></img>
+    </div>
+  );
+}
+
+export default App;
