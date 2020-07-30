79cd47562c847fd2970b0cb6d1d754257393b536
Adam Mathieson <mathieson1222@gmail.com>
2020-07-30 12:19:06 -0700

    added proxy to package.json to handle the cors error - doesnt look like it will solve for multiple api calls, will research a library to solve for

--- nrel-data/src/api/index.js
+++ nrel-data/src/api/index.js
@@ -1,21 +1,40 @@
 import axios from 'axios'
 
-const { REACT_NREL_API_KEY, REACT_BASE_URL } = process.env
+const { REACT_APP_NREL_API_KEY, REACT_APP_BASE_URL } = process.env
 
 const axiosConfig = {
-    baseURL: REACT_BASE_URL
-}
+    // baseURL: REACT_APP_BASE_URL,
+    proxy: {
 
+    }
+}
+// console.log( REACT_APP_BASE_URL)
 const requestWithAuth = () => {
     const instance = axios.create({
         ...axiosConfig,
-        headers: {'X-Api-Key': `${REACT_NREL_API_KEY}`}
+        headers: {
+            'X-Api-Key': `${REACT_APP_NREL_API_KEY}`,
+            // 'Content-Type': 'application/json'
+        }
     })
     return instance
 }
 
 export const getSolarData = async coord => {
-    const { data } = await requestWithAuth().get('/api/solar/solar_resource/v1.json', {coord})
+    const { data } = await requestWithAuth()
+        .get(`api/solar/solar_resource/v1.json/`, {
+            params: {
+                lat: coord.lat, 
+                lon: coord.lon 
+            }
+        })
     console.log(data)
     return data
 }
+
+
+let temp = {
+    lat: 40,
+    lon: -105
+}
+getSolarData(temp)
